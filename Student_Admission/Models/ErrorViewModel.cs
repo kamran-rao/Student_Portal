0a9f23446fc0befb1d2580ec905a2c8e3c931457
kamran-rao <kamranjaved0143@gmail.com>
2023-07-16 20:01:46 +0500

    Add project files.

---
+++ Student_Admission/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Student_Admission.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
